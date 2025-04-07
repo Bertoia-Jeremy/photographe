80ef1f1890a880f3e1d48cb6d137e48605c944f4
Jérémy Bertoïa <j.bertoia@debussac.net>
2025-04-07 22:47:23 +0200

    fix: change way to charge js

--- web/themes/custom/base_theme/assets/scripts/scripts.js
+++ web/themes/custom/base_theme/assets/scripts/scripts.js
@@ -3,9 +3,6 @@ import './menu_links_children.js';
 import './backToTop.js';
 import './disable_inspection.js';
 import slider from "./slider.js"
-import initLozad from "./initLozad.js"
-
-initLozad()
 
 document.addEventListener('DOMContentLoaded', () => {
     slider()
